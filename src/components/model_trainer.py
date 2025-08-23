b05ceb4f32792c6de72b18812871b6767ff563ff
Rahul Pandey <pandeyrahul8218@gmail.com>
2025-08-23 10:12:02 +0530

    deployment configuration

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,64 +1,8 @@
-
-## this ode to remove comes in dada_ingestion.py
-import os
-import sys
-from dataclasses import dataclass
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-
-from src.exception import CustomException
-from src.logger import logging
-from src.utils import save_object
-
-@dataclass
-class ModelTrainerConfig:
-    trained_model_file_path: str = os.path.join("artifacts", "model.pkl")
-
-class ModelTrainer:
-    def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
-
-    def initiate_model_trainer(self, train_array, test_array):
-        try:
-            logging.info("Splitting training and test input data")
-
-            X_train, y_train, X_test, y_test = (
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
-                test_array[:, -1],
-            )
-
-            # Example model (Linear Regression)
-            model = LinearRegression()
-            model.fit(X_train, y_train)
-
-            # Predictions
-            y_pred = model.predict(X_test)
-            score = r2_score(y_test, y_pred)
-
-            logging.info(f"Model R2 Score: {score}")
-
-            # Save model
-            save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=model,
-            )
-
-            return score
-
-        except Exception as e:
-            raise CustomException(e, sys)
-
-
-
-
-## main code 
 import os
 import sys
 from dataclasses import dataclass
 
-
+from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
@@ -68,7 +12,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-
+from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
@@ -98,7 +42,8 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             params={
